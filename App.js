d9864b224ad8461c50790d16b89235cda29e3bb4
Robbin Nurhuda <nurhudarobbin@gmail.com>
2020-11-18 21:48:07 +0800

    Tugas 13

--- App.js
+++ App.js
@@ -2,14 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import YoutubeUI from './Tugas/tugas12/App';
+import Login from './Tugas/tugas13/LoginScreen'
 
 export default function App() {
   return (
-    <YoutubeUI />
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
+    // <YoutubeUI />
+    <View style={styles.container}>
+      {/* <YoutubeUI /> */}
+      {/* <Text>123</Text> */}
+      {/* <Login/> */}
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
---
+++ Tugas/tugas13/LoginScreen.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+
+export default class LoginScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Login Screen</Text>
+      </View>
+    )
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'lightgreen'
+  }
+});
