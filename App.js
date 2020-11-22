bd645b8b27cf74f402672666b56eb2d3630321b9
Robbin Nurhuda <nurhudarobbin@gmail.com>
2020-11-22 23:31:39 +0800

    Quiz 3

--- App.js
+++ App.js
@@ -1,18 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import YoutubeUI from './Tugas/tugas12/App';
-import Login from './Tugas/tugas13/LoginScreen'
+
+// import Component from './Latihan/Component/Component';
+// import YoutubeUI from './Tugas/Tugas12/App';
+// import Noter from './Tugas/Tugas14/App';
+// import SkillScreen from './Tugas/Tugas14/SkillScreen'
+// import TugasNavigation from './Tugas/TugasNavigation/index'
+// import JawabanQuiz3 from './Backup/JawabanQuiz3/index'
+import Quiz3 from './Quiz3/index';
+
+import LatihanRedux from './Latihan/Redux/index'
+import SimpleRedux from './Latihan/Redux/SimpleRedux'
 
 export default function App() {
   return (
+    // <LatihanRedux />
+    // <SimpleRedux />
+    <Quiz3 />
+    // <JawabanQuiz3 />
+    // <TugasNavigation />
+    // <Tugas15 />
+    // <SkillScreen />
+    // <Noter />
     // <YoutubeUI />
-    <View style={styles.container}>
-      {/* <YoutubeUI /> */}
-      {/* <Text>123</Text> */}
-      {/* <Login/> */}
-      <StatusBar style="auto" />
-    </View>
+    // <Component />
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    // </View>
   );
 }
 
---
+++ src/store/reducers/todoReducers.js
@@ -0,0 +1,14 @@
+const initialState = {
+  todos: [],
+}
+
+const todoReducer = (state = initialState, action) => {
+  switch (action) {
+    case action.ADD:
+      // fungsi increment
+      break;
+  
+    default:
+      break;
+  }
+}
